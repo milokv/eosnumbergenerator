f7e763c1892d5d4e9051592e118ecf0bdf94716e
Milo <milo.kvarnberg@gmail.com>
2025-03-12 20:44:51 +0100

    added balance and double functionality

--- BJRNG.js
+++ BJRNG.js
@@ -1,8 +1,6 @@
 const API_URL = 'https://node1.eosphere.io/v1/chain'; // eos blockchain API URL, change if needed
 
 
-nonce = 0;
-let isGenerating = false;
 // deck of cards in order, for card generation
 const deck = [
     "2 ♠", "3 ♠", "4 ♠", "5 ♠", "6 ♠", "7 ♠", "8 ♠", "9 ♠", "10 ♠", "J ♠", "Q ♠", "K ♠", "A ♠",  // Spades (♠)
@@ -18,62 +16,81 @@ const deckValues = [
 ];
 // 12 25 38 51
 
+let balance = 1000;
+let betAmount = 0;
+let isGenerating = false;
+let nonce = 0;
 let playerHand = [];
 let dealerHand = [];
 let playerScore = 0;
 let dealerScore = 0;
-let playerBlackjack = false;
-let dealerBlackjack = false;
 let playerBust = false;
 let dealerBust = false;
 let playerTurn = false; // track if its players turn
+let playerCanSplit = false; // track if player can split
+let playerCanDouble = false; // track if player can double
 let playerAces = 0; // track if player used an ace to reduce score
 let playerConvertedAces = 0; // track how many aces were converted to 1
 let dealerAces = 0; // track if dealer used an ace to reduce score
 let dealerConvertedAces = 0; // track how many aces were converted to 1
+let generatedDeck = [];
+let currentCard = 3;
 
 document.getElementById("hitButton").addEventListener("click", playerHit);
 document.getElementById("stayButton").addEventListener("click", playerStay);
+document.getElementById("doubleButton").addEventListener("click", playerDouble);
+
+
+document.getElementById("balance").textContent = "Balance:  " + balance;
 
 async function startHand() {
     // reset game state
+    generatedDeck = [];
     playerHand = [];
     dealerHand = [];
     playerScore = 0;
     dealerScore = 0;
-    playerBlackjack = false;
-    dealerBlackjack = false;
     playerBust = false;
     dealerBust = false;
     playerTurn = true;
+    playerCanSplit = false;
+    playerCanDouble = false;
     playerAces = 0;
     playerConvertedAces = 0;
     dealerAces = 0;
     dealerConvertedAces = 0;
+    currentCard = 3;
+    betAmount = Number(document.getElementById('betAmount').value);
+    if (betAmount > balance) {
+        document.getElementById('result').textContent = `You don't have enough balance!`;
+        return;
+    }
+    balance -= betAmount;
+    document.getElementById('balance').textContent = `Balance: ${balance}`;
+    await generateMultipleNumbers(52);
+    console.log(generatedDeck);
     document.getElementById('playerHand').textContent = `Player Hand: `;
     document.getElementById('dealerHand').textContent = `Dealer Hand: `;
     document.getElementById('playerScore').textContent = `Player Score: 0`;
     document.getElementById('dealerScore').textContent = `Dealer Score: 0`;
     // deal first cards
     document.getElementById('result').textContent = `Dealing first player card...`;
-    playerHand.push(12);
+    playerHand.push(generatedDeck[0]);
     document.getElementById('playerHand').textContent = `Player Hand: ${deck[playerHand[0]]}`;
     document.getElementById('result').textContent = `Dealing first dealer card...`;
-    dealerHand.push(12);
+    dealerHand.push(generatedDeck[1]);    
     document.getElementById('dealerHand').textContent = `Dealer Hand: ${deck[dealerHand[0]]}, ?`;
     document.getElementById('result').textContent = `Dealing second player card...`;
-    playerHand.push(12);
+    playerHand.push(generatedDeck[2]);
     document.getElementById('playerHand').textContent = `Player Hand: ${deck[playerHand[0]]}, ${deck[playerHand[1]]}`;
     document.getElementById('result').textContent = `Dealing second dealer card...`;
-    dealerHand.push(12);
+    dealerHand.push(generatedDeck[3]);
 
     // calculate initial scores
     playerScore = deckValues[playerHand[0]] + deckValues[playerHand[1]];
     dealerScore = deckValues[dealerHand[0]];
 
-    // display initial hands
-    document.getElementById('playerHand').textContent = `Player Hand: ${deck[playerHand[0]]}, ${deck[playerHand[1]]}`;
-    document.getElementById('dealerHand').textContent = `Dealer Hand: ${deck[dealerHand[0]]}, ?`;
+    // display initial scores
     document.getElementById('playerScore').textContent = `Player Score: ${playerScore}`;
     document.getElementById('dealerScore').textContent = `Dealer Score: ${dealerScore}`; // hide full dealer score
     dealerScore = deckValues[dealerHand[0]] + deckValues[dealerHand[1]];
@@ -102,24 +119,34 @@ async function startHand() {
     } 
     else if (playerScore == 21 && dealerScore == 21) {
         document.getElementById('result').textContent = `It's a Tie!`;
+        playerTie();
     }
     else if (playerScore == 21) {
-        playerBlackjack = true;
         document.getElementById('result').textContent = `Player has Blackjack!`;
         playerTurn = false;
         revealDealerHand();
+        playerBlackjack();
         return;
+    }
+    else if (deckValues[playerHand[0]] == deckValues[playerHand[1]]) {
+        playerCanSplit = true;
+        playerCanDouble = true;
+        playerTurn = true;
+        document.getElementById('result').textContent = `Hit, Stay, Split or Double?`;
     }
     else {
         playerTurn = true;
-        document.getElementById('result').textContent = `Hit or Stay?`; // player's turn
+        playerCanDouble = true;
+        document.getElementById('result').textContent = `Hit, Stay or Double?`; // player's turn
     }
 }
 
 async function playerHit() {
     if (!playerTurn) return; // stop if player already stayed
+    playerCanDouble = false;
     document.getElementById('result').textContent = `Hitting...`;
-    let newCard = await generateRandomNumber();
+    currentCard++;
+    let newCard = generatedDeck[currentCard];
     playerHand.push(newCard);
     document.getElementById('result').textContent = `Hit or Stay?`;
     playerScore += deckValues[newCard];
@@ -130,13 +157,11 @@ async function playerHit() {
 
     // Check if player busts
     if (playerScore > 21) {
-        console.log("player score over 21, converting aces maybe");
 
         // Count the number of Aces in the hand
         for (let i in playerHand) {
             if (deckValues[playerHand[i]] == 11) {
                 playerAces++;
-                console.log("player aces: " + playerAces);
             }
         }
     
@@ -145,8 +170,6 @@ async function playerHit() {
             playerScore -= 10;
             playerAces = 0;
             playerConvertedAces++;
-            console.log("player aces after conversion" + playerAces);
-            console.log(`Ace converted! New Score: ${playerScore}`);
             document.getElementById('playerScore').textContent = `Player Score: ${playerScore}`;
         }
 
@@ -166,6 +189,66 @@ async function playerHit() {
     }
 }
 
+function playerDouble() {
+    if (!playerTurn) return;
+    if (!playerCanDouble) return;
+    playerCanDouble = false;
+    playerTurn = false;
+    if (betAmount > balance) {
+        document.getElementById('result').textContent = `You don't have enough balance!`;
+        return;
+    }
+    balance -= betAmount;
+    document.getElementById('balance').textContent = `Balance: ${balance}`;
+    betAmount *= 2;
+    let newCard = generatedDeck[currentCard];
+    playerHand.push(newCard);
+    document.getElementById('result').textContent = `Hit or Stay?`;
+    playerScore += deckValues[newCard];
+
+    // Update UI
+    document.getElementById('playerHand').textContent = `Player Hand: ${playerHand.map(card => deck[card]).join(', ')}`;
+    document.getElementById('playerScore').textContent = `Player Score: ${playerScore}`;
+
+    // Check if player busts
+    if (playerScore > 21) {
+
+        // Count the number of Aces in the hand
+        for (let i in playerHand) {
+            if (deckValues[playerHand[i]] == 11) {
+                playerAces++;
+            }
+        }
+    
+        // Convert Aces from 11 to 1 until the score is under 21
+        while (playerScore > 21 && playerAces > playerConvertedAces) {
+            playerScore -= 10;
+            playerAces = 0;
+            playerConvertedAces++;
+            document.getElementById('playerScore').textContent = `Player Score: ${playerScore}`;
+        }
+
+        if (playerScore > 21) {
+            playerBust = true;
+            document.getElementById('result').textContent = `Player Busted!`;
+            playerTurn = false;
+            revealDealerHand();
+            return;
+        }
+    }
+    if (playerScore == 21) {
+        playerTurn = false;
+        document.getElementById('result').textContent = `You have 21! Dealer's turn.`;
+        revealDealerHand();
+        dealerTurn();
+    }
+    else {
+        revealDealerHand();
+        dealerTurn();
+    }
+}
+
+
 function playerStay() {
     if (!playerTurn) return;
 
@@ -173,6 +256,21 @@ function playerStay() {
     document.getElementById('result').textContent = `You stayed. Dealer's turn.`;
     revealDealerHand();
     dealerTurn();
+}
+
+function playerWin() {
+    balance += 2 * betAmount;
+    document.getElementById('balance').textContent = `Balance: ${balance}`;
+}
+
+function playerTie() {
+    balance += betAmount;
+    document.getElementById('balance').textContent = `Balance: ${balance}`;
+}
+
+function playerBlackjack() {
+    balance += betAmount * 2.5;
+    document.getElementById('balance').textContent = `Balance: ${balance}`;
 }
 
 function revealDealerHand() {
@@ -184,16 +282,14 @@ async function dealerTurn() {
     let i = 0;
     while (dealerScore < 17) {
         document.getElementById('result').textContent = `Dealer Hitting...`;
-        dealerHand.push(await generateRandomNumber());
+        dealerHand.push(generatedDeck[currentCard + i]);
         dealerScore += deckValues[dealerHand[2+i]];
         if (dealerScore > 21) {
-            console.log("dealer score over 21, converting aces maybe");
 
             // Count the number of Aces in the hand
             for (let i in dealerHand) {
                 if (deckValues[dealerHand[i]] == 11) {
                     dealerAces++;
-                    console.log("dealer aces: " + dealerAces);
                 }
             }
         
@@ -202,8 +298,6 @@ async function dealerTurn() {
                 dealerScore -= 10;
                 dealerAces = 0; // Only remove 1 Ace at a time
                 dealerConvertedAces++;
-                console.log("dealer aces after conversion" + dealerAces);
-                console.log(`Ace converted! New Score: ${dealerScore}`);
                 document.getElementById('dealerScore').textContent = `Dealer Score: ${dealerScore}`;
             }
         }
@@ -216,12 +310,15 @@ async function dealerTurn() {
     if (dealerScore > 21) {
         dealerBust = true;
         document.getElementById('result').textContent = `Dealer Busted! You Win!`;
+        playerWin();
     } else if (dealerScore > playerScore) {
         document.getElementById('result').textContent = `Dealer Wins!`;
     } else if (dealerScore < playerScore) {
         document.getElementById('result').textContent = `You Win!`;
+        playerWin();
     } else {
         document.getElementById('result').textContent = `It's a Tie!`;
+        playerTie();
     }
 }
 
@@ -327,23 +424,21 @@ async function generateMultipleNumbers(amountOfNumbers) {
     isGenerating = true; // lock the function while already running
 
     try {
-        const nonceBefore = nonce;
-
         const clientSeed = document.getElementById('clientSeed').value || "7e6fc018"; // default seed is random hex
         const nextBlockHash = await fetchNextBlockHash();
-
-        while (nonce < nonceBefore + amountOfNumbers) {
-            const currentCombinedString = nextBlockHash + clientSeed + nonce; // add some random string to make the hash unique, + nonce and client seed
+        let i = 0;
+        while (i < amountOfNumbers) {
+            const currentCombinedString = nextBlockHash + clientSeed + i; // add some random string to make the hash unique, + nonce and client seed
             const currentHashedValue = await hashData(currentCombinedString);
             console.log(currentHashedValue);
             const currentRandomNumber = BigInt("0x" + currentHashedValue) % BigInt(52); // convert the hash to a number in the range
-            nonce = nonce + 1;
+            i++;
             const currentRandomInt = Number(currentRandomNumber);
             console.log(`Nonce: ${nonce}, Random Number: ${currentRandomNumber}, Hashed Value: ${currentHashedValue}`);
-            hands.push(currentRandomInt);
+            generatedDeck.push(currentRandomInt);
         }
+        nonce++;
         document.getElementById('nonce').textContent = `Nonce: ${nonce}`;
-        console.log(hands);
 
     } catch (error) {
         console.error('Error fetching data:', error);
